cd60584be3a6caa00490acd02075b07a90f0ff3f
Eli Temanson <temanson.eli@gmail.com>
2023-07-13 20:39:25 -0400

    added root cern as a dependence

--- single-det/src/kinemtatics.hh
+++ single-det/src/kinemtatics.hh
@@ -1,6 +1,6 @@
 
-#ifndef kinematics_hpp
-#define kinematics_hpp
+#ifndef kinematics_hh
+#define kinematics_hh
 
 //C and C++ libraries.
 #include <cmath>
@@ -14,51 +14,55 @@
 #include "TLorentzVector.h"
 #include "TVector3.h"
 
-struct Particle {
-    double M,InvMass,KE,E,Ecm;
-    double X,Y,Z;
-    double Theta,Phi;
-    double Px,Py,Pz,P;
-    TLorentzVector LV;
-    TVector3 Pos;
-};
+namespace kinematics {
 
-class Kinematics{
-protected:
-  // double mass[6];
-  double FragEst=0.0;
-  double BeamSigma=0.0, MeanBeamEnergy=0.0;
-  TRandom3 *Rndm = new TRandom3();
-  
-public:
-  Kinematics(){};
-  ~Kinematics(){};
+  struct Particle {
+      double M,InvMass,KE,E,Ecm;
+      double X,Y,Z;
+      double Theta,Phi;
+      double Px,Py,Pz,P;
+      TLorentzVector LV;
+      TVector3 Pos;
+  };
 
-  Particle Beam,Target,Ejectile,Fragment,Decay_Light,Decay_Heavy;
+  class Kinematics{
+  protected:
+    // double mass[6];
+    double FragEst=0.0;
+    double BeamSigma=0.0, MeanBeamEnergy=0.0;
+    TRandom3 *Rndm = new TRandom3();
+    
+  public:
+    Kinematics(){};
+    ~Kinematics(){};
 
-  void SetReaction(const std::string& beam,const std::string& target,const std::string& ejectile,const std::string& fragment,const std::string& product,const std::string& heavy);
- 
-  double QValue = 0.0;
-  double DecayQValueEst = 0.0;
-  double DecayQValue = 0.0;
-  double ExcEnergy = 0.0;
-  double InvMassExcEnergy = 0.0;
-  double RecoilQValue = 0.0;
-  double ThetaCM = 0.0;
-  double Sp = 0.0;
-  double gamma,term1,term2,term3;
-  double Ecm,Pcm;
+    Particle Beam,Target,Ejectile,Fragment,Decay_Light,Decay_Heavy;
 
-  void SetMeanBeamEnergy(const double&);
-  void SetBeamSigma(const double&);
-  void SetExcitationEnergy(const double&); 
-  double SeparationEnergy();
+    void SetReaction(int nucleus[][2]);
+    void SetReaction(const std::string& beam,const std::string& target,const std::string& ejectile,const std::string& fragment,const std::string& product,const std::string& heavy);
+   
+    double QValue = 0.0;
+    double DecayQValueEst = 0.0;
+    double DecayQValue = 0.0;
+    double ExcEnergy = 0.0;
+    double InvMassExcEnergy = 0.0;
+    double RecoilQValue = 0.0;
+    double ThetaCM = 0.0;
+    double Sp = 0.0;
+    double gamma,term1,term2,term3;
+    double Ecm,Pcm;
 
-  double r,s,Pcm_in,Pcm_out,rapidity;
-  bool Calc2Body(const double&);
-  bool Calc3Body();
+    void SetMeanBeamEnergy(const double&);
+    void SetBeamSigma(const double&);
+    void SetExcitationEnergy(const double&); 
+    double SeparationEnergy();
 
-};
+    double r,s,Pcm_in,Pcm_out,rapidity;
+    bool Calc2Body(const double&);
+    bool Calc3Body();
 
+  };
+
+}
 
 #endif
