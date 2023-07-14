dec2a82649c7c323315c1ea1a24bfbf05efaa158
eli-temanson <temanson.eli@gmail.com>
2023-07-14 14:35:35 -0400

    debugged GenericSD class

---
+++ generic-det/src/GenericSD.cc
@@ -0,0 +1,94 @@
+//===================================================================
+//  Sensitive Detector Class for Geant4 Simulation
+//
+//  This Class allows for a simple and convient way to extract 
+//      simulation data for a given detector. This method is nice 
+//      for single detector volumes but for larger project the Action
+//      Event and Stepping classes should be utilized.
+//
+//  To implement this class make sure you have added the 
+//      "ConstructSDandField" function to the Detector Construction.
+//
+//  Class includes: 
+//      > Initialize()
+//      > ProcessHits()
+//          > GetTotalEnergyDeposit()
+//      > EndOfEvent()
+//
+//  Date of Last Modified: 12/15/2021
+//  Author: Eli Temanson
+//===================================================================
+
+#include "GenericSD.hh"
+
+//===================================================================
+// GenericSD class.
+//
+
+GenericSD::GenericSD(G4String name) :
+  G4VSensitiveDetector(name), 
+  hitCollection(nullptr), 
+  HCID(-1) {
+  collectionName.insert("GenericHitsCollection");
+}
+
+GenericSD::~GenericSD() {}
+
+void GenericSD::Initialize(G4HCofThisEvent* hce) {
+  hitCollection = new GenericHitsCollection(GetName(), collectionName[0]);
+
+  static G4int hcid = GetCollectionID(0);  
+  hce->AddHitsCollection(hcid, hitCollection);
+  hitCollection->insert(new GenericHit());
+}
+
+G4bool GenericSD::ProcessHits(G4Step *step, G4TouchableHistory *ROhist) {
+  
+  G4double edep = step->GetTotalEnergyDeposit();
+  if(edep/eV < .1) return true;
+
+  G4String type = step->GetTrack()->GetDefinition()->GetParticleType();
+  if(type != "nucleus" && type != "baryon") return true;
+
+  G4StepPoint* preStepPoint = step->GetPreStepPoint();
+  G4StepPoint* postStepPoint = step->GetPostStepPoint();
+
+  G4TouchableHistory* touchable = (G4TouchableHistory*)(preStepPoint->GetTouchable());
+  G4int copyNo = touchable->GetVolume()->GetCopyNo();
+  G4double hitTime = preStepPoint->GetGlobalTime();
+  G4int trackID = step->GetTrack()->GetTrackID();
+
+  G4ThreeVector position = preStepPoint->GetPosition();
+
+  G4ParticleDefinition* particle = step->GetTrack()->GetDefinition();
+
+  GenericHit* hit = new GenericHit(copyNo, trackID, hitTime/ns, edep/MeV, position/mm, particle);
+  hitCollection->insert(hit);
+
+  return true;
+}
+
+void GenericSD::EndOfEvent(G4HCofThisEvent*) {}
+
+
+
+//===================================================================
+// GenericHit class.
+//
+
+G4ThreadLocal G4Allocator<GenericHit>* GenericHitAllocator;
+
+GenericHit::GenericHit() : 
+  G4VHit(), 
+  edep(0.0) {}
+
+GenericHit::GenericHit(G4int id, G4int trackID, G4double time, G4double energy, G4ThreeVector pos, G4ParticleDefinition* particle) : 
+    G4VHit(), 
+    ID(id), 
+    TrackID(trackID), 
+    Time(time), 
+    Energy(energy), 
+    Position(pos), 
+    Particle(particle) {}
+
+GenericHit::~GenericHit() {}
